be412982e4035377bdbd35d935f31d4ea968e344
nakayamaryo0731 <nakayamaryo0731@gmail.com>
2025-12-19 11:40:10 +0900

    Update Contacts (Japan, email, socials) and TimeZone (JST)

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,31 +2,22 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInItaly(): Date {
-  // Create a date object with the current UTC time
+export function getCurrentTimeInJapan(): Date {
   const now = new Date();
-
-  // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
-
   return now;
 }
 
-export function formatTimeForItaly(date: Date): string {
+export function formatTimeForJapan(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
-    hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    hour12: false,
+    timeZone: "Asia/Tokyo",
   };
 
-  let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
-
-  // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
-  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
+  let formattedTime = new Intl.DateTimeFormat("ja-JP", options).format(date);
+  formattedTime += " JST";
 
   return formattedTime;
 }
