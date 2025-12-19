606ec1de6ead67df7b19c79e2ae5a0127803459d
nakayamaryo0731 <nakayamaryo0731@gmail.com>
2025-12-19 10:29:25 +0900

    Switch from Netlify to Cloudflare Pages adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
@@ -30,7 +30,7 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime],
   },
   output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
+  adapter: cloudflare(),
   vite: {
     assetsInclude: "**/*.riv",
   },
